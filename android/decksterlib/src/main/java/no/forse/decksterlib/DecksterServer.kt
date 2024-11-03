4287fe2984b45d36180efe6f7e3ca15f50ecd3e0
Frode Nilsen <nilzor@gmail.com>
2024-11-03 22:14:29 +0100

    Convert login function to Retrofit instead of manual OkHttp

--- android/decksterlib/src/main/java/no/forse/decksterlib/DecksterServer.kt
+++ android/decksterlib/src/main/java/no/forse/decksterlib/DecksterServer.kt
@@ -6,56 +6,29 @@ import kotlinx.coroutines.suspendCancellableCoroutine
 import no.forse.decksterlib.authentication.LoginModel
 import no.forse.decksterlib.authentication.UserModel
 import no.forse.decksterlib.communication.MessageSerializer
-import no.forse.decksterlib.coroutines.safeResume
-import no.forse.decksterlib.coroutines.safeResumeWithException
-import okhttp3.Call
-import okhttp3.Callback
-import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import okhttp3.RequestBody.Companion.toRequestBody
-import okhttp3.Response
 import okhttp3.WebSocket
+import retrofit2.Retrofit
+import retrofit2.converter.jackson.JacksonConverterFactory
 import java.io.IOException
 
 class DecksterServer(
     private val hostAddress: String,
 ) {
-
-    // todo: Probably private, encapsulate functions better
-    val okHttpClient = OkHttpClient.Builder().build()
-    private val serializer = MessageSerializer()
+    private val okHttpClient = OkHttpClient.Builder().build()
+    private val api = Retrofit.Builder()
+        .baseUrl("http://$hostAddress")
+        .client(okHttpClient)
+        .addConverterFactory(JacksonConverterFactory.create(MessageSerializer.jackson))
+        .build()
+        .create(DecksterApi::class.java)
 
     fun getGameInstance(gameName: String, token: String): DecksterGame {
         return DecksterGame(this, gameName, token)
     }
 
-    suspend fun login(credentials: LoginModel): UserModel {
-        val authBody = serializer.serialize(credentials)
-        val requestBody = authBody.toRequestBody("application/json".toMediaType())
-        val request = Request.Builder()
-            .method("POST", requestBody)
-            .url("http://$hostAddress/login")
-            .build()
-        return suspendCancellableCoroutine<UserModel> { cont ->
-            okHttpClient.newCall(request).enqueue(object : Callback {
-                override fun onFailure(call: Call, e: IOException) {
-                    cont.safeResumeWithException(LoginFailedException(e))
-                }
-
-                override fun onResponse(call: Call, response: Response) {
-                    try {
-                        val responseStr = response.body?.string()
-                            ?: throw IOException("No body in response")
-                        val user = serializer.deserialize(responseStr, UserModel::class.java)
-                        cont.safeResume(user)
-                    } catch (ex: Exception) {
-                        cont.safeResumeWithException(ex)
-                    }
-                }
-            })
-        }
-    }
+    suspend fun login(credentials: LoginModel): UserModel = api.login(credentials)
 
     fun getRequest(path: String, token: String): Request {
         return Request.Builder()
