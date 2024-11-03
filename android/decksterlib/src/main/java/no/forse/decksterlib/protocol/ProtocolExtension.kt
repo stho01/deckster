7efa9e1a0fbb4dc5a92a594c70a5775364aec238
Frode Nilsen <nilzor@gmail.com>
2024-11-03 20:32:47 +0100

    Fix ProtocolExtension : Sending of message with right package name

--- android/decksterlib/src/main/java/no/forse/decksterlib/protocol/ProtocolExtension.kt
+++ android/decksterlib/src/main/java/no/forse/decksterlib/protocol/ProtocolExtension.kt
@@ -2,4 +2,4 @@ package no.forse.decksterlib.protocol
 
 import no.forse.decksterlib.model.protocol.DecksterMessage
 
-fun DecksterMessage.getType(): String = this.javaClass.simpleName.replace("XXX", ".")
+fun DecksterMessage.getType(): String = this.javaClass.name.replace("no.forse.decksterlib.model.", "")
